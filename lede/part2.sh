62deba0e4a4225ea8dbfd2cedc657d59b77b5651
jhzhao71 <16130314@qq.com>
2024-06-15 11:34:28 +0800

    Update part2.sh

--- lede/part2.sh
+++ lede/part2.sh
@@ -22,7 +22,7 @@
 # echo '=========Alert Kernel OK!========='
 
 # Alter default router IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.254/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #修改NTP设置
 #sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
